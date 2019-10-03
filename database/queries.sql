567e82611eefbd8368978136323488ebb205526f
breno <breno.ms1997@gmail.com>
2019-10-03 14:27:09 -0300

    adicionada query de inserção de usuario novo

--- database/queries.sql
+++ database/queries.sql
@@ -6,3 +6,7 @@ SELECT books.title FROM books WHERE books.release_year = 1900;
 
 -- Query se selecao de livros por autor
 SELECT books.title FROM books WHERE books.author LIKE '%$pattern%';
+
+
+INSERT INTO users($username,$name,$age,$passwdhash,$email) 
+VALUES ("teste","teste",21,"hashhash","teste@mail.com");
